613c1ebe84156aa110be440b978724e647a52206
yshuqiao <yshuqiao@qq.com>
2020-01-20 23:24:21 +0800

    '20200120'

---
+++ nqueens.java
@@ -0,0 +1,93 @@
+package myleetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class nqueens
+{
+
+	int rows[];
+	//"hill" diagonals
+	int hills[];
+	
+	//"dale" diagonals
+	int dales[];
+	int n;
+	//output
+	List<List<String>> output = new ArrayList();
+	// queens positions
+	int queens[];
+	
+	public boolean isNotUnderAttack(int row,int col) {
+		int res = rows[col] + hills[row - col + 2*n] + dales[row + col];
+		return (res == 0)?true:false;
+	}
+	
+	public void placeQueen(int row,int col) {
+		queens[row] = col;
+		rows[col] = 1;
+		hills[row - col +2*n] = 1; //"hill" diagonals
+		dales[row + col] = 1; //"dale" diagonals
+	}
+	public void removeQueen(int row,int col) {
+		queens[row] = 0;
+		rows[col] = 0;
+		hills[row - col +2*n] = 0;
+		dales[row + col] = 0;
+	}
+	
+	public void addSolution() {
+		List<String> solution = new ArrayList<String>();
+		for (int i=0;i<n;++i) {
+			int col = queens[i];
+			StringBuilder sb = new StringBuilder();
+			for(int j=0;j<col;++j) sb.append(".");
+			sb.append("Q");
+			for(int j=0;j<n-col-1;++j) sb.append(".");
+			solution.add(sb.toString());
+		}
+		output.add(solution);
+	}
+	
+	public void backtrack(int row) {
+		for(int col=0;col<n;col++) {
+			if(isNotUnderAttack(row,col)) {
+				placeQueen(row,col);
+				//if n queens are already placed
+				if(row+1==n) addSolution();
+				//if not proceed to place the rest
+				else backtrack(row+1);
+				//backtrack
+				removeQueen(row,col);
+			}
+		}
+	}
+	
+	public List<List<String>> solveNQueens(int n) {
+		this.n = n;
+		rows = new int[n];
+		hills = new int[4*n-1];
+		dales = new int[2*n-1];
+		queens = new int[n];
+		
+		backtrack(0);
+		return output;
+	}
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		nqueens Nqueens = new nqueens();
+		List<List<String>> solutions = Nqueens.solveNQueens(4);
+		
+		for (int i=0;i<solutions.size(); i++) {
+			List<String> aset = solutions.get(i);
+			System.out.println("第"+(i+1)+"层");
+			for (String a:aset) {
+				System.out.println(a);
+			}
+	    }	
+
+	}
+
+}
